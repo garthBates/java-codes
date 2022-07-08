26f7396337a4b6539cdcb15b410038ae0711c05e
seancarter99 <scarter@skillstorm.com>
2022-07-08 10:19:53 -0400

    Adding react-router-dom examples

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -1,38 +1,20 @@
-import { useState } from "react";
-import { ClassComponent } from "./components/ClassComponent";
-import { FunctionComponent } from "./components/FunctionComponent";
-import { SoftwareDeveloperList } from "./components/SoftwareDeveloper";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Day1, Day2, Home } from "./pages";
 
 // React believes that a function should determine the UI output
 
 // "named" export
 // Named exports are all exported in a singular object
 export const App = () => {
-    const [shouldRender, setShouldRender] = useState(false);
-    
-    // Whatever is returned from this function will be rendered to the page
     return (
-        // You may only return ONE JSX element per function
-        // This is a fragment. Fragments allow us to return multiple JSX elements
-        // They do NOT render to the webpage
-        <>
-            <button onClick={() => setShouldRender(!shouldRender)}>Toggle Components</button>
-            <h1>Hello React!</h1>
-            <h2>Hello Again!</h2>
-            <SoftwareDeveloperList />
-            
-            {/* Calling our ClassComponent this way will auto call render() on our behalf */}
-            {/* If shouldRender is true, render ClassComponent. If false, return false. False does NOT render */}
-            {/* This is known as "conditional rendering" */}
-            {shouldRender && <ClassComponent />}
-            {shouldRender ? <FunctionComponent /> : null}
-            {/* <ClassComponent /> */}
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/day1" element={<Day1 />} />
+                <Route path="/day2" element={<Day2 />} />
+            </Routes>
+        </BrowserRouter>
     );
-
-    // If we return multiple with no fragment, we'd try to return two things like this (which we can't do)
-    // React.createElement("h1", null, "Hello React!");
-    // React.createElement("h2", null, "Hello Again!");
 }
 
 export const two = 2;
---
+++ react-practice/src/pages/Home.jsx
@@ -0,0 +1,5 @@
+export const Home = () => {
+    return (
+        <h1>Welcome to my React website!</h1>
+    );
+}
